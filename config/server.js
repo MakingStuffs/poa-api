8734613a9c4af25b4c4cfa345c19b466269bc3d2
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-09-22 07:46:39 +0100

    -- Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '16a2350e9a4e0bb9177729b14807f529'),
+    },
+  },
+});
